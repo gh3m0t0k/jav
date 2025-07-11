eda54ac9afa1aceecd31cd98c73b6603533c5a06
gh3m0t0k <pascaradrian@yahoo.com>
2025-07-12 00:51:35 +0300

    commit

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -4,7 +4,7 @@ public class AddTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
+        System.out.print("Enter 1st number: ");
         int num1 = scanner.nextInt();
 
         System.out.print("Enter second number: ");
